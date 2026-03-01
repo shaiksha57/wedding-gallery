64c494d969c5beae8c8b495a5a5c7d5dd31bf05f
Shaik Shaikshavali <shaikzunnu143@gmail.com>
2026-03-01 20:26:55 +0530

    Fix production run issue

--- app.py
+++ app.py
@@ -222,5 +222,5 @@ def admin_view():
 
     return render_template('admin.html', users=users)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
